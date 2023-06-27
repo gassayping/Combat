fc9016de8aa84063fe65eae1792e8553f15b5859
Gassayping <Dev_Gassayping@proton.me>
2023-06-27 00:12:07 -0700

    Added sword combo abilities and fail message

--- src/Combat_BP/scripts/Main.js
+++ src/Combat_BP/scripts/Main.js
@@ -23,14 +23,29 @@ world.afterEvents.itemUse.subscribe((evd) => {
     const item = evd.itemStack;
     if (player.typeId !== 'minecraft:player' || !player.hasTag('swordPower'))
         return;
-    player.addEffect('speed', 35);
-    player.removeTag('swordPower');
+    switch (item.typeId.slice(-4)) {
+        case 'word':
+            swordPower(player);
+            break;
+        case '_axe':
+            console.warn('axe');
+            break;
+        default:
+            return;
+    }
 });
 function swordCombo(player) {
     const id = player.id;
     const current = swordCombos.set(id, (swordCombos.get(id) ?? 0) + 1).get(id);
+    if (current > 10) {
+        player.removeEffect('speed');
+        player.addEffect('weakness', 7, { amplifier: 255, showParticles: false });
+        player.onScreenDisplay.setActionBar('§cCombo Failed');
+        swordCombos.delete(player.id);
+        return;
+    }
     const counter = current % 5 === 0 ? 5 : current % 5;
-    const display = ''.padStart((5 - counter) * 4, '§b- ').padStart(5 * 4, '§ao ').concat('§g[]');
+    const display = ''.padStart((5 - counter) * 4, '§b- ').padStart(5 * 4, '§ao ').concat(`§${counter > 5 ? '4' : 'g'}[]`);
     player.removeTag('swordPower');
     if (counter === 5)
         player.addTag('swordPower');
@@ -38,7 +53,20 @@ function swordCombo(player) {
     system.runTimeout(() => {
         if (swordCombos.get(id) !== current)
             return;
-        swordCombos.set(id, 0);
+        swordCombos.delete(id);
         player.onScreenDisplay.setActionBar(' ');
-    }, 15);
+        player.removeTag('swordPower');
+    }, 20);
+}
+function swordPower(player) {
+    if (!player.removeTag('swordPower'))
+        return;
+    if (swordCombos.get(player.id) === 10) {
+        player.addEffect('strength', 35, { showParticles: false });
+        swordCombos.delete(player.id);
+    }
+    else {
+        player.addEffect('speed', 35, { showParticles: false });
+    }
+    player.onScreenDisplay.setActionBar('§ao o o o o §l§d#');
 }
